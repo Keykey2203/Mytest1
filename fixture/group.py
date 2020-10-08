c586f0614fe20a306d6b0090b324a4783d65baca
79120 <katya-student.yrgy@mail.ru>
2020-10-08 19:47:08 +0500

    Исправлено 7-ое задание

--- fixture/group.py
+++ fixture/group.py
@@ -33,11 +33,12 @@ class GroupHelper:
     def edit_group2(self, group):
         wd = self.app.wd
         self.open_groups_page()
+        wd.find_element_by_xpath("//input[@value='Edit group']").click()
         self.fill_info(group)
+        wd.find_element_by_name("update").click()
 
     def fill_info(self, group):
         wd = self.app.wd
-        wd.find_element_by_xpath("//input[@value='Edit group']").click()
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys(group.name)
@@ -47,7 +48,6 @@ class GroupHelper:
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys(group.footer)
-        wd.find_element_by_name("update").click()
 
     def open_groups_page(self):
         wd = self.app.wd
