af31cda40d324d61c229dbddfc0f9fb9ae480dcd
79120 <katya-student.yrgy@mail.ru>
2020-10-08 18:43:51 +0500

    change_field_value

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,7 +2,12 @@
 from model.group import Group
 
 
-def test_edit_group2(app):
+def test_edit_first_group2_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.edit_group2(Group(name="Новый", header="Новый", footer="Новый"))
+    app.group.edit_group2(Group(name="Новый"))
+    app.session.logout()
+
+def test_edit_first_group2_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.edit_group2(Group(header="Новый"))
     app.session.logout()
